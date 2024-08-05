fb709bbc2bed5d268efbd2c89462a1c0506fce40
leesanghoit <kop010504@gmail.com>
2024-08-05 19:01:25 +0900

    버튼 누르면 1씩 증가하는 기능 추가

--- src/main/resources/static/web/js/healthsign.js
+++ src/main/resources/static/web/js/healthsign.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     // 스트레칭 버튼
     const button1 = document.getElementById('delayedButton1');
+    const counter1 = document.getElementById('counter1'); // 숫자 표시할 요소
+
+    let count1 = 0;
+
     function activateButton1() {
         button1.disabled = false;
     }
@@ -11,9 +15,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     setTimeout(activateButton1, 1800 * 1000); // 페이지 로드 후 30분 뒤 버튼 활성화
-    button1.addEventListener('click', deactivateButton1);
+    button1.addEventListener('click', () => {
+        count1 += 1;
+        counter1.textContent = count1; // 숫자 업데이트
+        deactivateButton1();
+    });
+
     // 수분 보충 버튼
     const button2 = document.getElementById('delayedButton2');
+    const counter2 = document.getElementById('counter2'); // 숫자 표시할 요소
+
+    let count2 = 0;
+
     function activateButton2() {
         button2.disabled = false;
     }
@@ -24,9 +37,18 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     setTimeout(activateButton2, 3600 * 1000); // 페이지 로드 후 1시간 뒤 버튼 활성화
-    button2.addEventListener('click', deactivateButton2);
+    button2.addEventListener('click', () => {
+        count2 += 1;
+        counter2.textContent = count2; // 숫자 업데이트
+        deactivateButton2();
+    });
+
     // 마인드 컨트롤 버튼
     const button3 = document.getElementById('delayedButton3');
+    const counter3 = document.getElementById('counter3'); // 숫자 표시할 요소
+
+    let count3 = 0;
+
     function activateButton3() {
         button3.disabled = false;
     }
@@ -37,5 +59,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     setTimeout(activateButton3, 2400 * 1000); // 페이지 로드 후 40분 뒤 버튼 활성화
-    button3.addEventListener('click', deactivateButton3);
+    button3.addEventListener('click', () => {
+        count3 += 1;
+        counter3.textContent = count3; // 숫자 업데이트
+        deactivateButton3();
+    });
 });
